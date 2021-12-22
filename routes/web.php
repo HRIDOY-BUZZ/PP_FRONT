5727b11ee34214c18b352035f3af153e007fbcac
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2021-12-22 16:05:51 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/optimize', function () {
+    Artisan::call('optimize:clear');
+});
 
 Route::get('/', function () {
     return view('pages.home');
