e279b589791c97dde6e9a2406ee936428408f4f0
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2021-12-22 15:23:03 +0600

    Navbar updated

--- resources/views/components/header1.blade.php
+++ resources/views/components/header1.blade.php
@@ -1,19 +1,63 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
         <div class="container container-fluid">
+          <a class="navbar-brand" href="#" style="color:brown; font-weight:bold; font-size:25px">PricePond</a>
           <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="nav navbar-nav ms-auto">
+            <ul class="navbar-nav">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"> Categories  </a>
+                  <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#"> Dropdown item 1 </a></li>
+                  <li><a class="dropdown-item" href="#"> Dropdown item 2 &raquo; </a>
+                     <ul class="submenu dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Submenu item 1</a></li>
+                      <li><a class="dropdown-item" href="#">Submenu item 2</a></li>
+                      <li><a class="dropdown-item" href="#">Submenu item 3</a></li>
+                   </ul>
+                  </li>
+                  <li><a class="dropdown-item" href="#"> Dropdown item 3 &raquo; </a>
+                     <ul class="submenu dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Another submenu 1</a></li>
+                      <li><a class="dropdown-item" href="#">Another submenu 2</a></li>
+                      <li><a class="dropdown-item" href="#">Another submenu 3</a></li>
+                      <li><a class="dropdown-item" href="#">Another submenu 4</a></li>
+                   </ul>
+                  </li>
+                  <li><a class="dropdown-item" href="#"> Dropdown item 4 &raquo;</a>
+                     <ul class="submenu dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Another submenu 1</a></li>
+                      <li><a class="dropdown-item" href="#">Another submenu 2</a></li>
+                      <li><a class="dropdown-item" href="#">Another submenu 3</a></li>
+                      <li><a class="dropdown-item" href="#">Another submenu 4</a></li>
+                   </ul>
+                  </li>
+                  <li><a class="dropdown-item" href="#"> Dropdown item 5 </a></li>
+                  <li><a class="dropdown-item" href="#"> Dropdown item 6 </a></li>
+                  </ul>
+              </li>
+            </ul>
+            <div id="custom-search-input">
+              <div class="input-group col-md-12">
+                  <input type="text" class="  search-query form-control" placeholder="Search Items Here . . ." />
+                  <span class="input-group-btn">
+                      <button class="btn btn-danger" type="button">
+                        <i class="fas fa-search"></i>
+                      </button>
+                  </span>
+              </div>
+            </div>
+            <ul class="nav navbar-nav right-menu ms-auto">
               <li class="nav-item">
-                <a class="nav-link circled-item" href="#">Login</a>
+                <a class="nav-link circled-item" href="#"><i class="fas fa-user"></i></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link circled-item" href="#">Register</a>
+                <a class="nav-link circled-item" href="#"><i class="fas fa-user-plus"></i></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link circled-item" href="#">Deals</a>
+                <a class="nav-link circled-item" href="#"><i class="fas fa-handshake"></i></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link circled-item" href="#">Events</a>
+                <a class="nav-link circled-item" href="#"><i class="fas fa-calendar-alt"></i></a>
               </li>
             </ul>
           </div>
