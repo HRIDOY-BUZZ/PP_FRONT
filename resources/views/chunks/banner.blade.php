b2b36e04b79fd6bc67dec1dfc1185704d43007c1
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2022-01-05 17:00:59 +0600

    Slider Improved

--- resources/views/chunks/banner.blade.php
+++ resources/views/chunks/banner.blade.php
@@ -9,19 +9,19 @@
     </div>
     <div class="carousel-inner">
         <div class="carousel-item active"
-            style="background-image: url('https://source.unsplash.com/LAaSoL0LrYs/1920x1080')">
+            style="background-image: url('{{ asset("images/banners/banner-1.webp") }}')">
             {{-- <div class="carousel-caption">
                 <h5>First slide label</h5>
                 <p>Some representative placeholder content for the first slide.</p>
             </div> --}}
         </div>
-        <div class="carousel-item" style="background-image: url('https://source.unsplash.com/bF2vsubyHcQ/1920x1080')">
+        <div class="carousel-item" style="background-image: url('{{ asset("images/banners/banner-2.webp") }}')">
             {{-- <div class="carousel-caption">
                 <h5>Second slide label</h5>
                 <p>Some representative placeholder content for the second slide.</p>
             </div> --}}
         </div>
-        <div class="carousel-item" style="background-image: url('https://source.unsplash.com/szFUQoyvrxM/1920x1080')">
+        <div class="carousel-item" style="background-image: url('{{ asset("images/banners/banner-3.webp") }}')">
             {{-- <div class="carousel-caption">
                 <h5>Third slide label</h5>
                 <p>Some representative placeholder content for the third slide.</p>
