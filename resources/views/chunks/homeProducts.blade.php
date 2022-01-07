044c036e955dd18d674ef2e646356cb822bb13d4
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2022-01-07 18:43:58 +0600

    Product Designs Added

--- resources/views/chunks/homeProducts.blade.php
+++ resources/views/chunks/homeProducts.blade.php
@@ -9,32 +9,34 @@
     </div>
     <div class="products-body">
         <section class="center slider">
-            <div>
-                <img src="https://via.placeholder.com/200x300?text=DUMMY-PICTURE-1">
-            </div>
-            <div>
-                <img src="https://via.placeholder.com/200x300?text=DUMMY-PICTURE-2">
-            </div>
-            <div>
-                <img src="https://via.placeholder.com/200x300?text=DUMMY-PICTURE-3">
-            </div>
-            <div>
-                <img src="https://via.placeholder.com/200x300?text=DUMMY-PICTURE-4">
-            </div>
-            <div>
-                <img src="https://via.placeholder.com/200x300?text=DUMMY-PICTURE-5">
-            </div>
-            <div>
-                <img src="https://via.placeholder.com/200x300?text=DUMMY-PICTURE-6">
-            </div>
-            <div>
-                <img src="https://via.placeholder.com/200x300?text=DUMMY-PICTURE-7">
-            </div>
-            <div>
-                <img src="https://via.placeholder.com/200x300?text=DUMMY-PICTURE-8">
-            </div>
-            <div>
-                <img src="https://via.placeholder.com/200x300?text=DUMMY-PICTURE-9">
-            </div>
+            @for ($i=1;$i<10;$i++)
+            {
+                <div class="single-product">
+                    <div class="product-image">
+                        <img src="https://demo2.drfuri.com/martfury12/wp-content/uploads/sites/53/2013/06/18a-300x300.jpg">
+                    </div>
+                    <div class="shop-name">
+                        <p>ROBERT'S STORE</p>
+                    </div>
+                    <hr class="product-hr">
+                    <div class="product-details">
+                        <div class="product-title">
+                            <p class="">Samsung UHD TV 24 inch</p>
+                        </div>
+                        <div class="stars">
+                            @for ($s=0;$s<4;$s++)
+                                <i class="fas fa-star active-star"></i>
+                            @endfor
+                            <i class="fas fa-star inactive-star"></i>
+                            <span class="count">04</span>
+                        </div>
+                        <div class="product-price">
+                            <p class="price-amount"><span class="currency">$</span>599.60</p>
+                        </div>
+                    </div>
+                </div>
+            }
+                
+            @endfor
         </section>
     </div>
