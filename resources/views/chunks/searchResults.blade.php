ef0a0e40b6265a2ccac8809443f02a22c76dc29a
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2022-02-15 18:15:53 +0600

    Update searchResults.blade.php

--- resources/views/chunks/searchResults.blade.php
+++ resources/views/chunks/searchResults.blade.php
@@ -2,27 +2,29 @@
     $root = "https://www.pricepond.com.au/prod_img/";
 @endphp
 
-<div>
+<div class="container">
     <h6>Showing search result for query: <b>{{ $query }}</b></h6>
     <p>
         <div class="row">
             @foreach ($data as $d)
                 <div class="col-lg-3">
-                    <div class="single-product">
-                        <a href="{{ $d->URL }}">
-                            <div class="product-image">
-                                @if (isset($d->Image) && $d->Image != null && $d->Image!="")
-                                    <img src="{{ $root.$d->Shop."/".$d->Image }}">
-                                @else
-                                    <img src="{{ $d->ImageURL }}">
-                                @endif
+                    <div class="card single-product">
+                        <div class="container">
+                            <a href="{{ $d->URL }}">
+                                <div class="product-image card-img-top">
+                                    @if (isset($d->Image) && $d->Image != null && $d->Image!="")
+                                        <img src="{{ $root.$d->Shop."/".$d->Image }}">
+                                    @else
+                                        <img src="{{ $d->ImageURL }}">
+                                    @endif
+                                </div>
+                            </a>
+                            <div class="shop-name">
+                                <p>{{ substr($d->shop_name,0,22) }}</p>
                             </div>
-                        </a>
-                        <div class="shop-name">
-                            <p>{{ substr($d->shop_name,0,22) }}</p>
                         </div>
                         <hr class="product-hr">
-                        <div class="product-details">
+                        <div class="card-body product-details">
                             <div class="product-title">
                                 <a href="{{ $d->URL }}">
                                     <p class="">{{ $d->Name }}</p>
