4d1ad827b77c0491762c0df30b518d4ba852f332
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2022-02-22 15:43:54 +0600

    megamenu edited

---
+++ resources/views/chunks/megamenu.blade.php
@@ -0,0 +1,201 @@
+<ul class="navbar-nav h-100">
+    <li class="nav-item dropdown">
+        {{-- <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" >
+        Dropdown link
+        </a> --}}
+    <button class="no-click" disabled> <a id="navbarDropdownMenuLink" class="nav-link categories dropdown-toggle d-flex" href="#" data-bs-toggle="dropdown">
+        <p class="cat-title my-2">Categories <i class="fas fa-chevron-down"></i></p>
+    </a> </button>
+
+        <ul class="dropdown-menu dropdown-menu-1 " aria-labelledby="navbarDropdownMenuLink">
+            <!-- nested dropdown -->
+            <li class="nav-item  dropdown dropdown-2 ">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" >
+                Nested link-1
+                </a>
+                <ul class="dropdown-menu dropdown-menu-2 drop-first" aria-labelledby="navbarDropdownMenuLink">
+                <div class="row">
+                    <div class="col col-6 col-md-4 ">
+                        <li><a class="dropdown-item" href="#">Action of link-1</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">point-3</a></li>
+                        <li><a class="dropdown-item" href="#">point-4</a></li>
+                    </div>
+                    <div class="col col-6 col-md-4">
+                        <li><a class="dropdown-item" href="#">point-5</a></li>
+                        <li><a class="dropdown-item" href="#">point-6</a></li>
+                        <li><a class="dropdown-item" href="#">point-7</a></li>
+                        <li><a class="dropdown-item" href="#">point-8</a></li>
+                    </div>
+                    <div class="col col-6 col-md-4">
+                        <li><a class="dropdown-item" href="#">point-9</a></li>
+                        <li><a class="dropdown-item" href="#">point-10</a></li>
+                        <li><a class="dropdown-item" href="#">point-11</a></li>
+                        <li><a class="dropdown-item" href="#">point-12</a></li>
+                        <li><a class="dropdown-item" href="#">point-13</a></li>
+                        <li><a class="dropdown-item" href="#">point-14</a></li>
+                        <li><a class="dropdown-item" href="#">point-15</a></li>
+                        <li><a class="dropdown-item" href="#">point-16</a></li>
+                        <li><a class="dropdown-item" href="#">point-17</a></li>
+                        <li><a class="dropdown-item" href="#">point-18</a></li>
+                    </div>
+                </div>
+                    
+                </ul>
+            </li>
+            <li class="nav-item dropdown dropdown-2 ">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" >
+                Nested link-2
+                </a>
+                <ul class="dropdown-menu dropdown-menu-2" aria-labelledby="navbarDropdownMenuLink">
+                <div class="row">
+                    <div class="col col-6 col-md-4">
+                        <li><a class="dropdown-item" href="#">link-Action-2</a></li>
+                        <li><a class="dropdown-item" href="#">Another action-2</a></li>
+                        <li><a class="dropdown-item" href="#">point-2-3</a></li>
+                        <li><a class="dropdown-item" href="#">point-2-4</a></li>
+                    </div>
+                    <div class="col col-6 col-md-4">
+                        <li><a class="dropdown-item" href="#">point-9</a></li>
+                        <li><a class="dropdown-item" href="#">point-10</a></li>
+                        <li><a class="dropdown-item" href="#">point-11</a></li>
+                        <li><a class="dropdown-item" href="#">point-12</a></li>
+                        <li><a class="dropdown-item" href="#">point-13</a></li>
+                        <li><a class="dropdown-item" href="#">point-14</a></li>
+                        <li><a class="dropdown-item" href="#">point-15</a></li>
+                        <li><a class="dropdown-item" href="#">point-16</a></li>
+                        <li><a class="dropdown-item" href="#">point-17</a></li>
+                        <li><a class="dropdown-item" href="#">point-18</a></li>
+                    </div>
+                </div>
+                    
+                </ul>
+            </li>
+            <li class="nav-item dropdown dropdown-2 ">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" >
+                Nested link-3
+                </a>
+                <ul class="dropdown-menu dropdown-menu-2" aria-labelledby="navbarDropdownMenuLink">
+                <div class="row">
+                    <div class="col col-6 col-md-4">
+                        <li><a class="dropdown-item" href="#">nested-Action-3</a></li>
+                        <li><a class="dropdown-item" href="#">Another action-3</a></li>
+                        <li><a class="dropdown-item" href="#">point-3-3</a></li>
+                        <li><a class="dropdown-item" href="#">point-3-4</a></li>
+                    </div>
+                    <div class="col col-6 col-md-4">
+                        <li><a class="dropdown-item" href="#">point-5</a></li>
+                        <li><a class="dropdown-item" href="#">point-6</a></li>
+                        <li><a class="dropdown-item" href="#">point-7</a></li>
+                        <li><a class="dropdown-item" href="#">point-8</a></li>
+                    </div>
+                </div>
+                    
+                </ul>
+            </li>
+
+            <li class="nav-item dropdown dropdown-2 ">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" >
+                Nested link-4
+                </a>
+                <ul class="dropdown-menu dropdown-menu-2" aria-labelledby="navbarDropdownMenuLink">
+                    <div class="row">
+                        <div class="col col-6 col-md-4">
+                            <li><a class="dropdown-item" href="#">nl-link-4</a></li>
+                            <li><a class="dropdown-item" href="#">sction-4</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-3</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-4</a></li>
+                        </div>
+                        <div class="col col-6 col-md-4">
+                            <li><a class="dropdown-item" href="#">test-4-5</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-6</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-7</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-8</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-9</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-10</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-11</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-12</a></li>
+                            
+                        </div>
+                        <div class="col col-6 col-md-4">
+                            <li><a class="dropdown-item" href="#">point-4-5</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-6</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-7</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-8</a></li>
+                        </div>
+                    </div>
+                    
+                </ul>
+            </li>
+
+            <li class="nav-item dropdown dropdown-2 ">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" >
+                Nested link-5
+                </a>
+                <ul class="dropdown-menu dropdown-menu-2" aria-labelledby="navbarDropdownMenuLink">
+                    <div class="row">
+                        <div class="col col-6 col-md-4">
+                            <li><a class="dropdown-item" href="#">NL-5</a></li>
+                            <li><a class="dropdown-item" href="#">sction-4</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-3</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-4</a></li>
+                        </div>
+                        <div class="col col-6 col-md-4">
+                            <li><a class="dropdown-item" href="#">test-4-5</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-6</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-7</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-8</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-9</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-10</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-11</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-12</a></li>
+                            
+                        </div>
+                        <div class="col col-6 col-md-4">
+                            <li><a class="dropdown-item" href="#">point-4-5</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-6</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-7</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-8</a></li>
+                        </div>
+                    </div>
+                    
+                </ul>
+            </li>
+
+            <li class="nav-item dropdown dropdown-2 ">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" >
+                Nested link-6
+                </a>
+                <ul class="dropdown-menu dropdown-menu-2" aria-labelledby="navbarDropdownMenuLink">
+                    <div class="row">
+                        <div class="col col-6 col-md-4">
+                            <li><a class="dropdown-item" href="#">link-6</a></li>
+                            <li><a class="dropdown-item" href="#">sction-4</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-3</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-4</a></li>
+                        </div>
+                        <div class="col col-6 col-md-4">
+                            <li><a class="dropdown-item" href="#">test-4-5</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-6</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-7</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-8</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-9</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-10</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-11</a></li>
+                            <li><a class="dropdown-item" href="#">test-4-12</a></li>
+                            
+                        </div>
+                        <div class="col col-6 col-md-4">
+                            <li><a class="dropdown-item" href="#">point-4-5</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-6</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-7</a></li>
+                            <li><a class="dropdown-item" href="#">point-4-8</a></li>
+                        </div>
+                    </div>
+                    
+                </ul>
+            </li>
+
+        </ul>
+    </li>
+</ul>
