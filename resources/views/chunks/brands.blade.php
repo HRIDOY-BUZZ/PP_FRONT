a1e4f0cebd20c18d8f8f4daa1d726b4f5fb58c6b
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2022-03-05 17:59:22 +0600

    brands updated

--- resources/views/chunks/brands.blade.php
+++ resources/views/chunks/brands.blade.php
@@ -2,18 +2,11 @@
     <div id="brands">
         <div class="products-body">
             <section class="slider brands">
-                @for ($i=1;$i<6;$i++)
+                @for ($i=1;$i<=16;$i++)
                     <div class="brand-div">
                         <div class="single-product">
                             <div class="brand-image">
-                                <img src="{{ asset('images/stores/nike.jpg') }}">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="brand-div">
-                        <div class="single-product">
-                            <div class="brand-image">
-                                <img src="{{ asset('images/stores/puma.jpg') }}">
+                                <img src="{{ asset('images/brands/'.$i.'.png') }}">
                             </div>
                         </div>
                     </div>
