d39c07b99018c3291cba20f7b55a8ccba80ccd21
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2022-03-09 15:47:47 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/optimize', function () {
     Artisan::call('optimize');
 });
-Route::get('/optimize', function () {
+Route::get('/optimize-clear', function () {
     Artisan::call('optimize:clear');
 });
 
