bbfd22f4de594ecd77fb86e0dfe0f07e46a03eb5
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2022-03-15 19:44:41 +0600

    deals page url added

--- resources/views/body/allDealsPage.blade.php
+++ resources/views/body/allDealsPage.blade.php
@@ -9,32 +9,40 @@
         @foreach ($data as $d)
             <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
                 <div class="blocks">
-                    <div class="card">
-                        <div class="card-img-top">
-                            <span>
-                                <img src="{{ $root.$d->Image }}" alt="{{ $d->shop_name }}">
-                            </span>
-                        </div>
-                        <div class="card-body">
-                            <div class="card-title">
-                                <h3>
-                                    @if(strlen($d->shop_name)>12)
-                                        {{ substr($d->shop_name,0,12) }}...
-                                    @else
-                                        {{ $d->shop_name }}
-                                    @endif
-                                </h3>
-                                @if($d->discount_type == 'percent')
-                                    <span class="coupon percent">{{ $d->discount }}% OFF</span>
-                                @elseif($d->discount_type == 'cash')
-                                    <span class="coupon shipping">FREE SHIPPING</span>
-                                @endif
+                    <a href="
+                    @if($d->URL != NULL && $d->URL != "")
+                        {{ $d->URL }}
+                    @else
+                        {{ $d->shop_url }}
+                    @endif
+                    ">
+                        <div class="card">
+                            <div class="card-img-top">
+                                <span>
+                                    <img src="{{ $root.$d->Image }}" alt="{{ $d->shop_name }}">
+                                </span>
                             </div>
-                            <p class="card_text">
-                                {{ $d->title }}
-                            </p>
+                            <div class="card-body">
+                                <div class="card-title">
+                                    <h3>
+                                        @if(strlen($d->shop_name)>12)
+                                            {{ substr($d->shop_name,0,12) }}...
+                                        @else
+                                            {{ $d->shop_name }}
+                                        @endif
+                                    </h3>
+                                    @if($d->discount_type == 'percent')
+                                        <span class="coupon percent">{{ $d->discount }}% OFF</span>
+                                    @elseif($d->discount_type == 'cash')
+                                        <span class="coupon shipping">FREE SHIPPING</span>
+                                    @endif
+                                </div>
+                                <p class="card_text">
+                                    {{ $d->title }}
+                                </p>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
                 <br>
             </div>
