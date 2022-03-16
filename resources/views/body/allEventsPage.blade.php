e9b7867ecae9e5fdba46b9ffb4fccbb82ca94fdb
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2022-03-16 13:05:35 +0600

    All Events page designed

--- resources/views/body/allEventsPage.blade.php
+++ resources/views/body/allEventsPage.blade.php
@@ -1,3 +1,7 @@
+@php
+    $root = "https://www.pricepond.com.au/event_images/";
+    $placeholder = "event_ph.jpg";
+@endphp
 <div class="container mt-5">
     <center>
         <h1><b>PRICEPOND EVENTS</b></h1>
@@ -7,7 +11,34 @@
         @foreach ($data as $d)
             @if($d->status == 1)
                 <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                    {{ print_r($d) }}
+                    <div class="blocks">
+                        <a href="{{ url('event/'.$d->id."/".encodeUrl($d->event_name)) }}">
+                            <div class="card event">
+                                <div class="card-img-top">
+                                    <span>
+                                        @if($d->image != NULL && $d->image != "")
+                                            <img src="{{ $root.$d->image }}" alt="{{ $d->event_name }}">
+                                        @else
+                                            <img src="{{ $root.$placeholder }}" alt="{{ $d->event_name }}">
+                                        @endif
+                                    </span>
+                                </div>
+                                <div class="card-body">
+                                    <div class="card-title">
+                                        <h3>
+                                            @if(strlen($d->event_name)>20)
+                                                {{ substr($d->event_name,0,20) }}...
+                                            @else
+                                                {{ $d->event_name }}
+                                            @endif
+                                        </h3>
+                                    </div>
+                                    <p class="card_text">
+                                    </p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
                     <br>
                 </div>
             @endif
@@ -18,7 +49,32 @@
         @foreach ($data as $d)
             @if($d->status != 1)
                 <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                    {{ print_r($d) }}
+                    <div class="blocks">
+                            <div class="card event">
+                                <div class="card-img-top">
+                                    <span>
+                                        @if($d->image != NULL && $d->image != "")
+                                            <img src="{{ $root.$d->image }}" alt="{{ $d->event_name }}">
+                                        @else
+                                            <img src="{{ $root.$placeholder }}" alt="{{ $d->event_name }}">
+                                        @endif
+                                    </span>
+                                </div>
+                                <div class="card-body">
+                                    <div class="card-title">
+                                        <h3>
+                                            @if(strlen($d->event_name)>20)
+                                                {{ substr($d->event_name,0,20) }}...
+                                            @else
+                                                {{ $d->event_name }}
+                                            @endif
+                                        </h3>
+                                    </div>
+                                    <p class="card_text">
+                                    </p>
+                                </div>
+                            </div>
+                    </div>
                     <br>
                 </div>
             @endif
