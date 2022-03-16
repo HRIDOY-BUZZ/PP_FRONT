d9c8e5137e83acf4d34d29c388f55b8f2e884921
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2022-03-16 17:36:53 +0600

    contact-us form designed

--- resources/views/body/contactBody.blade.php
+++ resources/views/body/contactBody.blade.php
@@ -9,7 +9,20 @@
             If you have any enquiries regarding the PricePond site, the services we provide, or would like to advertise with us please contact us using the contact form below.
         </p>
         <p>
-            <b>Note:</b> PricePond does not sell any products displayed on the site, certified online stores who advertise their products on PricePond do. If you require more information on a certain product please click on the item you are interested in and contact the online store that sells that item.
+            <b>Note:</b> PricePond does not sell any products displayed on the site, certified online stores who advertise their products on PricePond. If you require more information on a certain product please click on the item you are interested in and contact the online store that sells that item.
+        </p>
+    </div>
+    @include('chunks.contactForm')
+    <div class="contact-data">
+        <p>
+            <b>Alternatively you can contact us via:</b>
+        </p>
+        <p>
+            <b>Tel:</b>
+            <a href="tel:0280912748">02 8091 2748</a>
+        </p>
+        <p>
+            <b>Bookmark us to ensure you never miss out!</b>
         </p>
     </div>
 </div>
---
+++ resources/views/chunks/contactForm.blade.php
@@ -0,0 +1,92 @@
+<div class="contact-form my-5">
+    <form action="" method="post" class="form-control">
+        <center>
+            <h2 class="my-4">CONTACT FORM</h2>
+        </center>
+        <div class="container px-4">
+            <div class="row my-4">
+                <div class="col-md-6 col-sm-12">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-12">
+                            <label for="department" class="form-label">Department</label>
+                        </div>
+                        <div class="col-md-8 col-sm-12">
+                            <select name="department" id="department" class="form-select w90" required>
+                                <option value="contact@pricepond.com.au">CONTACT</option>
+                                <option value="enquiry@pricepond.com.au">ENQUIRY</option>
+                                <option value="support@pricepond.com.au">SUPPORT</option>
+                                <option value="info@pricepond.com.au">INFO</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <p class="form-text">
+                        <i>
+                            * A mail containing your message will be sent to the selected department of PricePond.
+                        </i>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-sm-12">
+                    <div class="row my-4">
+                        <div class="col-md-4 col-sm-12">
+                            <label for="name" class="form-label">Name / Store</label>
+                        </div>
+                        <div class="col-md-8 col-sm-12">
+                            <input type="text" name="name" id="name" class="form-control w90" placeholder="Your Name or Your Store Name" required>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <div class="row my-4">
+                        <div class="col-md-4 col-sm-12">
+                            <label for="email" class="form-label">E-mail Address</label>
+                        </div>
+                        <div class="col-md-8 col-sm-12">
+                            <input type="email" name="email" id="email" class="form-control w90" placeholder="Your E-mail Address" required>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row my-4">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-md-2 col-sm-12">
+                            <label for="subject" class="form-label">Subject</label>
+                        </div>
+                        <div class="col-md-10 col-sm-12">
+                            <input type="text" name="subject" id="subject" class="form-control w96" placeholder="E-mail Subject" required>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row my-4">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-md-2 col-sm-12">
+                            <label for="message" class="form-label">Message</label>
+                        </div>
+                        <div class="col-md-10 col-sm-12">
+                            <textarea name="message" id="message" rows="10" class="form-control w96" placeholder="Type your message in detail here..." required>
+
+                            </textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row my-4">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-md-2 col-sm-12">
+                        </div>
+                        <div class="col-md-10 col-sm-12">
+                            <input type="submit" name="submit" id="submit" class="btn btn-primary btn-pp w-25 py-2" value="SUBMIT">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
